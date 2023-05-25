32885e1a813a04661c70b4d414e942ae53b1bcc4
Chiara Scheurer <scch1091@h-ka.de>
2023-05-25 15:06:11 +0200

    Bildanpassungen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
      <img src='../homescreen_picture.jpg' 
-     width="1000"
-     height="auto"/>
+     width="120%"
+     height="800"/>
+
+.layer {
+    background-color: rgba(248, 247, 216, 0.7);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
     </main>
   )
 }
