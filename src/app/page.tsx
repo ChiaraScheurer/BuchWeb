c9f85210f184159aa5b2aa855be0a80368ca9ff1
Chiara Scheurer <scch1091@h-ka.de>
2023-05-29 19:06:22 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,12 @@
 import styles from './page.module.css'
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.layer}>
      <img src='../homescreen_picture.jpg' 
      width="120%"
      height="800"/>
 
-.layer {
-    background-color: rgba(248, 247, 216, 0.7);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-}
+
     </main>
   )
 }
